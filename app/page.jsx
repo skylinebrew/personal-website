da2a44fa47c5726839a1a2d4c33a0e15baf74d93
Clara Rossetti <mariaclaraqrq@gmail.com>
2025-07-28 22:36:38 -0300

    primeiras imagens na home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,17 @@
 
 export default function Home() {
   return (
-    <div className="flex relative h-screen">
-      <h1 className="text-5xl font-mono text-right absolute top-20 left-70 right-2.5">Clara Rossetti</h1>
-  
+    <div className="h-screen bg-blue-200">
+      <div className="flex relative l-screen">
+        <img src="/home/lua.webp" alt="lua" className="w-44 h-34 md:w-66 md:h-51 md:mt-5 -mr-20 md:ml-5 -ml-4" />
+        <img src="/home/logo.webp" alt="Clara Rossetti" className="w-84 h-60 ml-auto md:mr-80 mr-15 -mt-5"/>
+      </div>
+      <div className="flex relative l-screen">
+        <img src="/home/fone.webp" alt="Fone de ouvido" className="w-25 h-25 md:ml-50 ml-20 -mt-1" />
+        <img src="/home/livros.webp" alt="Estante de livros" className="w-48 h-49 ml-auto mr-5 -mt-20" />
+      </div>
+      
+      
     </div>
   );
 }
